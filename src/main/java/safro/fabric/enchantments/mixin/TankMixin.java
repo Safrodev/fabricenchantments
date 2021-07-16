1105b6596ac213af1d1b1463dfc3b26073b40204
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-16 14:14:05 -0400

    Updated with rework

--- src/main/java/safro/fabric/enchantments/mixin/TankMixin.java
+++ src/main/java/safro/fabric/enchantments/mixin/TankMixin.java
@@ -3,8 +3,13 @@ package safro.fabric.enchantments.mixin;
 
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.*;
+import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,19 +17,33 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import safro.fabric.enchantments.FabricEnchantments;
 
-@Mixin(PlayerEntity.class)
+@Mixin(LivingEntity.class)
     public abstract class TankMixin {
 
     @Shadow public abstract ItemStack getEquippedStack(EquipmentSlot slot);
 
     @Shadow public abstract void setAbsorptionAmount(float amount);
 
+
+    @Shadow public abstract float getMaxHealth();
+
+    @Shadow public abstract float getHealth();
+
+    @Shadow public abstract void setHealth(float health);
+
+    @Shadow public abstract AttributeContainer getAttributes();
+
+    @Shadow @Final private AttributeContainer attributes;
+
+    @Shadow @Nullable public abstract EntityAttributeInstance getAttributeInstance(EntityAttribute attribute);
+
     @Inject(at = @At("HEAD"), method = "tick", cancellable = true)
 
     public void TankArmor(CallbackInfo ci) {
 
-        if((EnchantmentHelper.getLevel(FabricEnchantments.TANK, this.getEquippedStack(EquipmentSlot.CHEST)) >= 1)) {
-            this.setAbsorptionAmount(1.0F * (EnchantmentHelper.getLevel(FabricEnchantments.TANK, this.getEquippedStack(EquipmentSlot.CHEST))));
+        if ((EnchantmentHelper.getLevel(FabricEnchantments.TANK, this.getEquippedStack(EquipmentSlot.CHEST)) >= 1)) {
+
+            this.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(20 + (EnchantmentHelper.getLevel(FabricEnchantments.TANK, this.getEquippedStack(EquipmentSlot.CHEST))));
         }
     }
 }
